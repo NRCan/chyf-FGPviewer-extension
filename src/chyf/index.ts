d338337ad9f850686cf78c56088c7f27d50bea8b
davebrochu15 <43046538+davebrochu15@users.noreply.github.com>
2019-04-10 17:18:54 -0400

    Fix(Pourpoint); Fix pourpoint name

--- src/chyf/index.ts
+++ src/chyf/index.ts
@@ -15,7 +15,7 @@ import { PourpointExtension } from "./PourpointExtension";
         const flowpathDownstream: Extension = new CHyFExtension(api, "Flowpaths Downstream", "http://dev.geogratis.gc.ca:8012/chyf/eflowpath/downstreamOf.json");
         const upstream: Extension = new CHyFExtension(api, "Drainages Upstream","http://dev.geogratis.gc.ca:8012/chyf/drainageArea/upstreamOf.json");
         const downstream: Extension = new CHyFExtension(api, "Drainages Downstream","http://dev.geogratis.gc.ca:8012/chyf/drainageArea/downstreamOf.json");
-        const pourpoint: Extension = new PourpointExtension(api, "Pourpoint", "http://chyf.ca/chyf/pourpoint/compute.json");
+        const pourpoint: Extension = new PourpointExtension(api, "Pourpoints", "http://chyf.ca/chyf/pourpoint/compute.json");
 
         manageExtension.addHTMLComponent(`<div class="list-extensions">Remove Holes: <input id="removeHoles" type="checkbox" value="holes"></div>`);
         manageExtension.addExtensions([upstream, downstream, flowpathUpstream, flowpathDownstream, pourpoint]);
